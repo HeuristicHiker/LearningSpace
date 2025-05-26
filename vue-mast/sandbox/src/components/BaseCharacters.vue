3e78991ec7453236942ccc565cc94a1ebce2a37e
HeuristicHiker <connor.dev.johnston@gmail.com>
2025-05-26 12:44:38 -0400

    addition of vue course

---
+++ vue-mast/sandbox/src/components/BaseCharacters.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  data() {
+    return {
+      newCharacter: '',
+      message: 'Hello it works',
+      listOfCharacters: [
+        {
+          name: 'Ang',
+          isFavorite: true,
+        },
+        {
+          name: 'Bob',
+          isFavorite: false,
+        },
+        {
+          name: 'Sally',
+          isFavorite: false,
+        },
+      ],
+    }
+  },
+
+}
+</script>
+
+<template>
+    <ul v-for="(item, index) in listOfCharacters" :key="`item-${index}`">
+      <li>
+        {{ item.name }}
+      </li>
+      <button @click="toggleFavorite(item)">Favorite {{ item.isFavorite }}</button>
+    </ul>
+    <hr />
+    <ul v-for="(item, index) in listOfCharacters.filter((item) => item.isFavorite)" :key="`number-${index}`">
+      <li>
+        {{ item.name }}
+      </li>
+      <button @click="toggleFavorite(item)">Favorite {{ item.isFavorite }}</button>
+    </ul>
+    <input @keyup.enter="() => console.log(x)" v-model.text="newCharacter" />
+    <button @click="addCharacter(newCharacter)">Add: {{ newCharacter }}</button>
+</template>
---
+++ vue-mast/sandbox/src/components/BaseCounter.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 10,
+      counterTitle: 'Counter Standard',
+      incrementAmount: 8,
+    }
+  },
+  computed: {
+    displayTitle() {
+      if (this.count > 20) {
+        return 'Counter Standard - Very Long'
+      } else {
+        return 'Counter Standard'
+      }
+    },
+  },
+  methods: {
+    incrementCount() {
+      this.count += this.incrementAmount
+    },
+    toggleFavorite(item, event) {
+      console.log(item)
+      item.isFavorite = !item.isFavorite
+    },
+    addCharacter(character) {
+      const newChar = { name: character, isFavorite: false }
+      this.listOfCharacters.push(newChar)
+      console.log(this.listOfCharacters)
+    },
+  }
+}
+
+</script>
+
+<template>
+    <h1>Hello Frontend Masters!</h1>
+    <h1>{{ counterTitle }}</h1>
+    <p :data-increment-by="incrementAmount">{{ count }}</p>
+    <button @click="incrementCount">Increment Count</button>
+    <div>
+      <label for="incrementAmount">Increment by:</label>
+      <input v-model.number="incrementAmount" />
+    </div>
+</template>
